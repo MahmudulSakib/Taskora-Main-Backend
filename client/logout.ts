c38486b8d88c1cabb91d996b41a3d11d02ad9de9
Mahmudul Sakib <mahmudulsakib.110@gmail.com>
2025-07-21 17:45:49 +0600

    added

--- client/logout.ts
+++ client/logout.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import passport from "../security/passportconfig";
 
 const clientLogout = express.Router();
 
-clientLogout.post("/client/log-out", (req, res) => {
-  res.clearCookie("clienttoken");
-  res.status(200).json({ message: "Logged out successfully" });
-});
+clientLogout.post(
+  "/client/log-out",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.clearCookie("clienttoken");
+    res.status(200).json({ message: "Logged out successfully" });
+  }
+);
 
 export default clientLogout;
